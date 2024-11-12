39c6b56f75de411486001ff8e721a55d94fc3f9c
Vlad Golubev <holybay1886@gmail.com>
2024-11-12 18:11:59 +0300

    Added DB sql scripts.

---
+++ postgres/user_service/sql/00_database.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE user_service
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LOCALE_PROVIDER = 'libc'
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
---
+++ postgres/user_service/sql/01_ddl.sql
@@ -0,0 +1,19 @@
+CREATE SCHEMA app
+    AUTHORIZATION postgres;
+
+CREATE TABLE app.users
+(
+id uuid,
+mail character varying NOT NULL,
+full_name character varying NOT NULL,
+role character varying NOT NULL,
+status character varying NOT NULL,
+password character varying NOT NULL,
+created_at TIMESTAMP NOT NULL,
+updated_at TIMESTAMP NOT NULL,
+CONSTRAINT users_pk PRIMARY KEY (id),
+CONSTRAINT users_mail_unq UNIQUE(mail)
+);
+
+ALTER TABLE IF EXISTS app.users
+    OWNER to postgres;
