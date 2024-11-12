22bbab07749fad9ff4df9ab2de3ff3b9bad2a187
Vlad Golubev <holybay1886@gmail.com>
2024-11-12 18:10:47 +0300

    Moved user_service to SpringBoot 3.3.5 and adjusted pom.xml accordingly.

---
+++ user_service/src/main/java/by/it_academy/jd2/finance/user_service/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package by.it_academy.jd2.finance.user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+}
---
+++ user_service/src/test/java/by/it_academy/jd2/finance/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.it_academy.jd2.finance.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
