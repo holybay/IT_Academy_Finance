faab71a3a3b6cd9d612b1f70ccc9810ed5858d15
Vlad Golubev <holybay1886@gmail.com>
2024-11-12 18:18:22 +0300

    Created UserMapper class.

---
+++ user_service/src/main/java/by/it_academy/jd2/finance/user_service/service/mapper/UserMapper.java
@@ -0,0 +1,49 @@
+package by.it_academy.jd2.finance.user_service.service.mapper;
+
+import by.it_academy.jd2.finance.user_service.repository.entity.User;
+import by.it_academy.jd2.finance.user_service.service.dto.UserCreateDto;
+import by.it_academy.jd2.finance.user_service.service.dto.UserOutDto;
+import by.it_academy.jd2.finance.user_service.service.dto.UserUpdateDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+
+    public User toEntity(UserCreateDto createDto) {
+        return User.builder()
+                   .setId(createDto.getId())
+                   .setCreatedAt(createDto.getCreatedAt())
+                   .setUpdatedAt(createDto.getUpdatedAt())
+                   .setMail(createDto.getMail())
+                   .setFio(createDto.getFio())
+                   .setRole(createDto.getRole())
+                   .setStatus(createDto.getStatus())
+                   .setPassword(createDto.getPassword())
+                   .build();
+    }
+
+    public User toEntity(UserUpdateDto updateDto) {
+        return User.builder()
+                   .setId(updateDto.getId())
+                   .setUpdatedAt(updateDto.getUpdatedAt())
+                   .setMail(updateDto.getMail())
+                   .setFio(updateDto.getFio())
+                   .setRole(updateDto.getRole())
+                   .setStatus(updateDto.getStatus())
+                   .setPassword(updateDto.getPassword())
+                   .build();
+    }
+
+    public UserOutDto toUserOutDto(User entity) {
+        return UserOutDto.builder()
+                         .setId(entity.getId())
+                         .setCreatedAt(entity.getCreatedAt())
+                         .setUpdatedAt(entity.getUpdatedAt())
+                         .setMail(entity.getMail())
+                         .setFio(entity.getFio())
+                         .setRole(entity.getRole())
+                         .setStatus(entity.getStatus())
+                         .build();
+    }
+
+}
